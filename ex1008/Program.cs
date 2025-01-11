a087555bc0184b2947279a74bdbaf16e15e7bbfc
tibor <fiedlerneto@gmail.com>
2025-01-10 21:40:09 -0300

    study C#

---
+++ ex1008/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+     int numero = int.Parse(Console.ReadLine());
+      int horas = int.Parse(Console.ReadLine());
+       double salary = double.Parse(Console.ReadLine());
+       
+       double salario = horas * salary;
+       
+       Console.WriteLine("NUMBER = " + numero);
+        Console.WriteLine("SALARY = U$ " +salario.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+}
